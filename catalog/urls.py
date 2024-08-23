afbeda2b3694bef021203835f62dfa0e54a55f8b
MARIA-AZRAR <mariaazrar2000@gmail.com>
2024-08-23 13:09:03 +0500

    added movies list by genre and added link to home

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,6 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', MovieListView.as_view(), name='movies'), 
+    path('<slug:genre_slug>/', MovieListView.as_view(), name='movie_list_by_genre'),
     path('<slug:movie>/<int:year>/<int:month>/<int:day>/', MovieDetailView.as_view(), name='movie_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
+from taggit.models import Tag
 from .models import Movie
 
 # Create your views here.
@@ -8,6 +11,28 @@ class MovieListView(ListView):
     model = Movie
     context_object_name = 'movies'
     template_name = 'catalog/movie/list.html'
+    
+    # override queryset to pass the movies with the slug only
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        genre_slug = self.kwargs.get('genre_slug')
+        
+        if genre_slug:
+            genre = get_object_or_404(Tag, slug=genre_slug)
+            queryset = queryset.filter(genres__in = [genre])
+        
+        return queryset
+    
+    # override the context data to pass genre slug to the template as well
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)   # run the parent method to get the movies object
+        genre_slug = self.kwargs.get('genre_slug')
+        context['genre'] = None
+        if genre_slug:
+            context['genre'] = get_object_or_404(Tag, slug=genre_slug)
+        return context
+            
+        
 
 class MovieDetailView(DetailView):
     model = Movie
